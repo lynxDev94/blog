d7396a87b9eca665cd8216eb4d6a52bae3f1e6eb
lynxDev94 <lynxpounceweb@gmail.com>
2025-06-06 02:15:15 +0300

    add new node env

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,4 +5,5 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const fetchUrl = 'http://localhost:3000/api'
+export const fetchUrl = process.env.NODE_ENV === "development"? 'http://localhost:3000/api' : 'http://blog-neon-nine-36.vercel.app'
+//  if not dev, should be page url
