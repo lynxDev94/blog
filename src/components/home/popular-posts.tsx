ac7ccf509d0bb5e716ef168394a5007b4c571e08
lynxDev94 <lynxpounceweb@gmail.com>
2025-06-06 12:46:23 +0300

    query data with swr

--- src/components/home/popular-posts.tsx
+++ src/components/home/popular-posts.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
-import { popularPosts } from '@/lib/placeholder-data';
-import { Icons } from '../ui/icons';
+'use client';
+
+import React from "react";
+import { Icons } from "../ui/icons";
+import useSWR from 'swr'
+
+
+import { fetcher, fetchUrl } from "@/lib/utils";
+import Link from "next/link";
 
 function PopularPosts() {
+  const { data, error, isLoading } = useSWR(fetchUrl, fetcher);
+
+  if (error) return <div>Failed to load... </div>;
+  if (isLoading) return <div>Loading...</div>;
+
   return (
-   <ul className='overflow-auto'>
-        {popularPosts.map((post) => (
-            <li key={post.title} className='flex items-center gap-2 group cursor-pointer py-2'>
-                    <Icons.ArrowRight className='h-6 w-6 group-hover:translate-x-1 transition-all'/>
-                    <p>{post.title}</p>  
-            </li>
-        ))}
-   </ul>
-  )
+    <ul className="overflow-auto">
+      {data?.map((post: {category: string, title: string, slug: string}) => (
+        <Link href={`blog/${post.category}/${post.slug}`} key={post.title}>
+        <li
+          key={post.title}
+          className="flex items-center gap-2 group cursor-pointer py-2"
+        >
+          <Icons.ArrowRight className="h-6 w-6 group-hover:translate-x-1 transition-all" />
+          <p>{post.title}</p>
+        </li>
+        </Link>
+      ))}
+    </ul>
+  );
 }
 
 export default PopularPosts;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,17 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+type ResponseData = {
+  category: string;
+  title: string;
+  slug: string;
+}[];
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 export const fetchUrl = process.env.NODE_ENV === "development"? 'http://localhost:3000/api' : 'https://blog-neon-nine-36.vercel.app/api'
-//  if not dev, should be page url
+
+export const fetcher = (...args: Parameters<typeof fetch>) : Promise<ResponseData> => fetch(...args).then(res => res.json())
+
